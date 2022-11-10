283292a7922d9b3064b73912be562c663b3f2853
SandhyaRaju01 <117722081+SandhyaRaju01@users.noreply.github.com>
2022-11-10 19:55:45 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -17,10 +17,10 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 #create the repeatable code block (called a function)
-def get_fruityvice_data(fruit_choice)
-     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     return fruityvice_normalized
+def get_fruityvice_data(this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
     
 streamlit.header("Fruityvice Fruit Advice!")
 try:
