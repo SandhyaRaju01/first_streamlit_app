97cbef784c73aae51001d272c2edcda976ba0b43
SandhyaRaju01 <117722081+SandhyaRaju01@users.noreply.github.com>
2022-11-10 19:38:48 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -28,8 +28,7 @@ else:
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-   streamlit.error()
+
 
 import snowflake.connector
 
